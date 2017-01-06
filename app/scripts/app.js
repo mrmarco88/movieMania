f7703e96ead99649720c93be6762a60f2e43b43d
Administrator <mrmarco88@gmail.com>
2017-01-06 19:35:45 +0100

    Primo commit, gulp funzionante

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name movieManiaApp
+ * @description
+ * # movieManiaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('movieManiaApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
